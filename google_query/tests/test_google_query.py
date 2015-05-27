2794462536dc6b44ae39d51d680c6620cd51fafc
Galkin Sergey <galkinsergeyv@gmail.com>
2015-05-27 08:35:23 +0300

    fixgoogle_query params tests

--- google_query/tests/test_google_query.py
+++ google_query/tests/test_google_query.py
@@ -16,37 +16,37 @@ class GoogleParserTests(GoogleQueryTests):
         self.assertEqual(q.get_url(), 'https://www.google.ru/search?q=%D1%81%D1%82%D0%BE%D0%BB&num=10&as_dt=e&near=%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0')
 
     def test3(self):
-        q = GoogleQuery('ru', 'стол', region='Москва', page=0)
+        q = GoogleQuery('ru', 'стол', region='Москва', start=0)
         self.assertEqual(q.get_url(), 'https://www.google.ru/search?q=%D1%81%D1%82%D0%BE%D0%BB&num=10&as_dt=e&near=%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0')
 
     def test4(self):
-        q = GoogleQuery('ru', 'стол', region='Москва', page=1)
+        q = GoogleQuery('ru', 'стол', region='Москва', start=1)
         self.assertEqual(q.get_url(), 'https://www.google.ru/search?q=%D1%81%D1%82%D0%BE%D0%BB&num=10&as_dt=e&near=%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0')
 
     def test5(self):
-        q = GoogleQuery('ru', 'стол', region='Москва', page=2)
+        q = GoogleQuery('ru', 'стол', region='Москва', start=2)
         self.assertEqual(q.get_url(), 'https://www.google.ru/search?q=%D1%81%D1%82%D0%BE%D0%BB&num=10&start=10&as_dt=e&near=%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0')
 
     def test6(self):
-        q = GoogleQuery('ru', 'стол', region='Москва', page=2, num=50)
+        q = GoogleQuery('ru', 'стол', region='Москва', start=2, num=50)
         self.assertEqual(q.get_url(), 'https://www.google.ru/search?q=%D1%81%D1%82%D0%BE%D0%BB&num=50&start=50&as_dt=e&near=%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0')
 
     def test7(self):
-        q = GoogleQuery('ru', 'стол', region='Москва', page=2, num=50)
+        q = GoogleQuery('ru', 'стол', region='Москва', start=2, num=50)
         self.assertEqual(q.get_url(), 'https://www.google.ru/search?q=%D1%81%D1%82%D0%BE%D0%BB&num=50&start=50&as_dt=e&near=%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0')
 
     def test8(self):
-        q = GoogleQuery('com', 'стол', region='Washington', page=2, num=50)
+        q = GoogleQuery('com', 'стол', region='Washington', start=2, num=50)
         self.assertEqual(q.get_url(), 'https://www.google.com/search?q=%D1%81%D1%82%D0%BE%D0%BB&num=50&start=50&hl=en&gl=US&gr=US-UT&gcs=NewYork&as_dt=e&near=Washington')
 
     def test9(self):
-        q = GoogleQuery('info', 'стол', region='Washington', page=2, num=50)
+        q = GoogleQuery('info', 'стол', region='Washington', start=2, num=50)
         self.assertEqual(q.get_url(), 'https://www.google.info/search?q=%D1%81%D1%82%D0%BE%D0%BB&num=50&start=50&as_dt=e&near=Washington')
 
     def test10(self):
-        q = GoogleQuery('info', 'стол', region='Washington', page=2, num=50, custom_params='oq=table&psi=1231231231')
+        q = GoogleQuery('info', 'стол', region='Washington', start=2, num=50, custom_params='oq=table&psi=1231231231')
         self.assertEqual(q.get_url(), 'https://www.google.info/search?q=%D1%81%D1%82%D0%BE%D0%BB&num=50&start=50&as_dt=e&near=Washington&oq=table&psi=1231231231')
 
     def test11(self):
-        q = GoogleQuery('com.uag', 'стол', region='Washington', page=2, num=50, custom_params='oq=table&psi=1231231231')
+        q = GoogleQuery('com.uag', 'стол', region='Washington', start=2, num=50, custom_params='oq=table&psi=1231231231')
         self.assertEqual(q.get_url(), 'https://www.google.com.ua/search?q=%D1%81%D1%82%D0%BE%D0%BB&num=50&start=50&hl=ru&tbs=ctr:countryUA&cr=countryUA&as_dt=e&near=Washington&oq=table&psi=1231231231')
