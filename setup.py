ea528ffd3034a3ac5758eaa146855224d836078a
Galkin Sergey <galkinsergeyv@gmail.com>
2015-06-22 10:49:38 +0300

    format

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description="Convert html to snippets",
     version=VERSION,
     url='https://github.com/KokocGroup/google-parser',
-    download_url='https://github.com/KokocGroup/google-parser/tarball/v{}'.format(VERSION),
+    download_url='https://github.com/KokocGroup/google-parser/tarball/v{0}'.format(VERSION),
     packages=['google_parser', 'google_query'],
     install_requires=[
         'pyquery==1.2.9',
